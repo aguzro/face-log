938df289543fc73e9d98571885fe7523865eab22
Allison Guzmán <allison@laboratoria.cl>
2017-07-04 13:38:18 -0400

    Agregado estados de logueado y no logueado

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    if (response.status === 'connected') {
+      testAPI();
+    } else {
+      var padre = document.getElementById('status');
+      padre.innerHTML = "";
+      var hijo = document.createElement("p");
+      var texto = document.createTextNode("Por favor, " + " ingresa a la app");
+      hijo.appendChild(texto);
+      padre.appendChild(padre);  
+    }
+  }
+
+
+function checkLoginState() {
+	FB.getLoginStatus(function(response) {
+		statusChangeCallback(response);
+	});
+}
+
 window.fbAsyncInit = function() {
 	FB.init({
 		appId      : '685469858315867',
@@ -21,9 +43,17 @@ FB.getLoginStatus(function(response) {
 	statusChangeCallback(response);
 });
 
+function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      var padre = document.getElementById('status');
+      padre.innerHTML = "";
+      var hijo = document.createElement("p");
+      var texto = document.createTextNode('¡Gracias por loguearte en ' + response.name + '!');
+      hijo.appendChild(texto);
+      padre.appendChild(padre);  
+    });
+  }
 
-function checkLoginState() {
-	FB.getLoginStatus(function(response) {
-		statusChangeCallback(response);
-	});
-}
+
