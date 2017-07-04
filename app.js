581972b5777eb6338d0fefc1fddf9bac4f65ce0e
Allison Guzmán <allison@laboratoria.cl>
2017-07-04 16:06:36 -0400

    Cambiando funciones

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
-function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
-    if (response.status === 'connected') {
-      testAPI();
-    } else {
-      var padre = document.getElementById('status');
-      padre.innerHTML = "";
-      var hijo = document.createElement("p");
-      var texto = document.createTextNode("Por favor, " + " ingresa a la app");
-      hijo.appendChild(texto);
-      padre.appendChild(padre);  
-    }
-  }
+	  // Esto es llamado con los resultados de FB.getLoginStatus().
+	  function statusChangeCallback(response) {
+	  	console.log('statusChangeCallback');
+	  	console.log(response);
+		// El objeto de respuesta se retorna con un campo de estado que permite a la
+		// app conocer el estado actual de inicio de sesión de la persona.
+		if (response.status === 'connected') {
+	      // Sesión iniciada en tu aplicación y en Facebook.
+	      testAPI();
+	  } else if (response.status === 'not_authorized') {
+	      // La persona esta loggeada en Facebook, pero no en la app.
+	      document.getElementById('status').innerHTML = 'Please log ' +
+	      'into this app.';
+	  } else {
+	      // La persona no ha iniciado sesión en Facebook, por lo que no estamos seguros de si
+		  // están conectados a esta aplicación o no.
+		  document.getElementById('status').innerHTML = 'Please log ' +
+		  'into Facebook.';
+		}
+	}
 
 
-function checkLoginState() {
+	function checkLoginState() {
+		FB.getLoginStatus(function(response) {
+			statusChangeCallback(response);
+		});
+	}
+
+
+
+
+	window.fbAsyncInit = function() {
+		FB.init({
+			appId      : '685469858315867',
+			cookie     : true,
+			xfbml      : true,
+			version    : 'v2.9'
+		});
+		FB.AppEvents.logPageView();   
+	};
+
+
 	FB.getLoginStatus(function(response) {
 		statusChangeCallback(response);
 	});
-}
-
-window.fbAsyncInit = function() {
-	FB.init({
-		appId      : '685469858315867',
-		cookie     : true,
-		xfbml      : true,
-		version    : 'v2.9'
-	});
-	FB.AppEvents.logPageView();   
 };
+
 
 (function(d, s, id){
 	var js, fjs = d.getElementsByTagName(s)[0];
@@ -40,16 +56,10 @@ window.fbAsyncInit = function() {
 
 
 function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-      console.log('Successful login for: ' + response.name);
-      var padre = document.getElementById('status');
-      padre.innerHTML = "";
-      var hijo = document.createElement("p");
-      var texto = document.createTextNode('¡Gracias por loguearte en ' + response.name + '!');
-      hijo.appendChild(texto);
-      padre.appendChild(padre);  
-    });
-  }
-
-
+	console.log('Welcome!  Fetching your information.... ');
+	FB.api('/me', function(response) {
+		console.log('Successful login for: ' + response.name);
+		document.getElementById('status').innerHTML =
+		'Thanks for logging in, ' + response.name + '!';
+	});
+}
